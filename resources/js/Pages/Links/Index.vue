87e19f0ad07c1a7672dee97650f70ed553d3bbf3
Bram Nijssen <bramnijssen28@gmail.com>
2024-01-26 19:23:34 +0100

    Add drag and drop list

--- resources/js/Pages/Links/Index.vue
+++ resources/js/Pages/Links/Index.vue
@@ -5,7 +5,9 @@ import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
 import { Head } from "@inertiajs/vue3";
 import LinkToggle from "../../Components/Links/LinkToggle.vue";
 
+let draggable;
 const jnmActive = ref(true);
+const draggableClasses = ["border-2", "border-dashed", "!bg-transparent", "text-transparent"];
 
 const props = defineProps({
     links: Array,
@@ -19,6 +21,28 @@ const dashboardLinksLength = computed(() => dashboardLinks.value.length);
 function style(hex) {
     return `background-color: ${hex}`;
 }
+
+function dragStart(e) {
+    draggable = e.target;
+    // Avoid displaying classes on draggable
+    setTimeout(() => draggable.classList.add(...draggableClasses));
+}
+
+function dragEnd() {
+    draggable.classList.remove(...draggableClasses);
+    draggable = null;
+}
+
+function dragOver(e) {
+    const target = e.target;
+    if (target.closest("li") !== draggable) {
+        const position = draggable.compareDocumentPosition(target);
+        target.parentNode.insertBefore(
+            draggable,
+            position === Node.DOCUMENT_POSITION_PRECEDING ? target : target.nextSibling
+        );
+    }
+}
 </script>
 
 <template>
@@ -28,19 +52,23 @@ function style(hex) {
         <LinkToggle @toggle="(e) => (jnmActive = e)"></LinkToggle>
 
         <div class="p-6 border border-t-0 border-gray-500 rounded-b-lg text-xl text-white flex flex-col gap-12">
-            <div v-if="dashboardLinksLength" class="flex flex-col gap-4">
-                <div
+            <ul v-if="dashboardLinksLength" class="flex flex-col gap-4">
+                <li
                     v-for="link in dashboardLinks"
                     :key="link.id"
-                    class="min-w-full px-6 py-3 rounded-lg flex gap-6 items-center"
                     :style="style(link.hex)"
+                    class="min-w-full px-6 py-3 rounded-lg border-gray-500 cursor-grab flex gap-6 items-center"
+                    draggable="true"
+                    @dragstart="dragStart"
+                    @dragend="dragEnd"
+                    @dragover="dragOver"
                 >
-                    <i class="fa-solid fa-bars"></i>
+                    <i class="fa-solid fa-grip-vertical"></i>
                     <span class="flex-1">{{ link.title }}</span>
                     <button class="fa-solid fa-trash-can hover:scale-90 duration-300"></button>
-                </div>
-            </div>
-            <div v-else class="italic text-black">Er zijn geen links</div>
+                </li>
+            </ul>
+            <span v-else class="italic text-black">Er zijn geen links</span>
 
             <div class="flex justify-end">
                 <a :href="route('links.create')"
